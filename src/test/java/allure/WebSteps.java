5559731dfad21dfede269e951e18ef32aac910aa
Andrey <butor.2011@mail.ru>
2025-09-17 23:32:21 +0700

    add file

---
+++ src/test/java/allure/WebSteps.java
@@ -0,0 +1,36 @@
+package allure;
+
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com/");
+    }
+
+    @Step("Ищем репозиторий ")
+    public void searchForRepo(String repo) {
+        $(".header-search-button").click();
+        $("#query-builder-test").setValue(repo).submit();
+    }
+
+    @Step("Ищем нужную issue ")
+    public void openIssueTab(String repo) {
+        $(linkText(repo)).click();
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
